271b3526b50d3db48a1baaf3b1adcfcfcd5c4f57
arunm5341 <arunm5341@gmail.com>
2021-07-21 17:55:23 +0530

    Essential bracket added

--- src/test/scala/TestMainQuestion.scala
+++ src/test/scala/TestMainQuestion.scala
@@ -6,6 +6,5 @@ class TestMainQuestion extends AnyFunSuite with BeforeAndAfterEach{
   //assert(MainQuestion.dateFormat(2021-12-23)==2021-12-23)
 
 assert(MainQuestion.LogLine(debug_level ="abc", timestamp= ???, download_id =12 , retrieval_stage ="xyz" , rest ="mnc" )===
-  ("abc" , ???,12,"xyz","mnc")
-)
+  ("abc" , ???,12,"xyz","mnc"))
 }
