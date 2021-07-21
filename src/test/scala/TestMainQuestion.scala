c2f860055a9aeeb2783c0c49a6e508008911ef8f
arunm5341 <arunm5341@gmail.com>
2021-07-21 10:48:24 +0530

    New test cases added

--- src/test/scala/TestMainQuestion.scala
+++ src/test/scala/TestMainQuestion.scala
@@ -3,7 +3,9 @@ import org.scalatest._
 import org.scalatest.funsuite.AnyFunSuite
 
 class TestMainQuestion extends AnyFunSuite with BeforeAndAfterEach{
-assert(MainQuestion.LogLine(debug_level =, timestamp = , download_id = , retrieval_stage = , rest = )===
-  LogLine(debug_level = ,timestamp = ???, download_id = ???, retrieval_stage = ???, rest = ???)
+  //assert(MainQuestion.dateFormat(2021-12-23)==2021-12-23)
+
+assert(MainQuestion.LogLine(debug_level ="abc", timestamp= ???, download_id =12 , retrieval_stage ="xyz" , rest ="mnc" )===
+  ("abc" , ???,12,"xyz","mnc")
 )
 }
